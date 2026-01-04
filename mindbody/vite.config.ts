f72ce95cd42083fc139e1db1a78e2c5cebd4cd38
Sriniketh Nadella <srinikethn@gmail.com>
2026-01-03 21:57:34 -0500

    build: regenerate docs/mindbody with correct base

--- mindbody/vite.config.ts
+++ mindbody/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/mindbody/",
+  base: "/docs/mindbody/",
   plugins: [react()],
   build: {
     outDir: "../docs/mindbody",
